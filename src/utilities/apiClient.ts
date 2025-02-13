94f0c5c3772801bbd4d093a174c7c13bab47f5e0
TarunSingh04 <tarun.singh@impakter.com>
2025-02-13 13:13:47 +0000

    Update apiClient.ts

--- src/utilities/apiClient.ts
+++ src/utilities/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URI || 'http://localhost:8000', // Replace with FastAPI URL
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URI || 'https://api.impakter.pro', // Replace with FastAPI URL
   headers: {
     'Content-Type': 'application/json',
   },
